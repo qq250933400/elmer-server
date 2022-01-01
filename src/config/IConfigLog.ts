8587dd2e928e7a569d1fc90bcb69982e5e9d6cd6
elmer s j mo <250933400@qq.com>
2022-01-01 18:52:24 +0800

    update config

--- src/config/IConfigLog.ts
+++ src/config/IConfigLog.ts
@@ -1,3 +1,7 @@
 export interface IConfigLog {
     level: "debug" | "info" | "error";
+    type: "file" | "multiFile" | "stdout";
+    timeout?: number;
+    extension?: string;
+    category?: string;
 }
