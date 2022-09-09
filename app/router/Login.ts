a11504c43bf7d5eab01b852a7c13fddbf0dc9c8a
elmer s j mo <250933400@qq.com>
2022-09-09 23:36:57 +0800

    update test api

--- app/router/Login.ts
+++ app/router/Login.ts
@@ -1,11 +1,87 @@
-import { Controller, RequestMapping, DataModel,QueryParam,RequestCookie } from "../../src";
-import { Autowired } from "elmer-common";
-import { GetResponse, RequestHeader, StaticFiles } from "../../src/core";
+import {
+    Controller,
+    RequestMapping,
+    DataModel,
+    QueryParam,
+    RequestCookie,
+    DefineDataModel,
+    GetDataModel,
+    GetServerConfig,
+    IConfigServer,
+    StaticFiles
+} from "../../src";
+import { Autowired, queueCallFunc, utils } from "elmer-common";
+import { GetResponse, RequestHeader } from "../../src/core";
 import { Request, Response } from "express";
+import * as path from "path";
+import * as fs from "fs";
 
+@DefineDataModel({
+    source: "doc.mysql"
+})
 class LoginModel extends DataModel {
+    @GetServerConfig
+    sevConfig!: IConfigServer;
+    @Autowired()
+    fileObj!: StaticFiles;
     dataSource() {
-        return "sso.mysql";
+        return new Promise((resolve, reject) => {
+            const rootPath = path.resolve(process.cwd(), this.sevConfig.temp);
+            const filePath = path.resolve(rootPath, "./doc");
+            const fileName = path.resolve(filePath, "database.md");
+            const mdText: string[] = [];
+            this.fileObj.checkDir(rootPath,  process.cwd());
+            this.fileObj.checkDir(filePath,  rootPath);
+            queueCallFunc([
+                {
+                    id: "tables",
+                    fn: () => {
+                        return this.query("showAllTables", {});
+                    }
+                }, {
+                    id: "columns",
+                    fn: (opt) => {
+                        const tables = opt.lastResult || [];
+                        const vParams:any[] = [];
+                        for(const table of tables) {
+                            const name = table["TABLE_NAME"], comment = table["TABLE_COMMENT"];
+                            vParams.push({
+                                id: name,
+                                params: {
+                                    name,
+                                    comment
+                                }
+                            });
+                        }
+                        
+                        return queueCallFunc(vParams, (opt, param): any => {
+                            return new Promise((_resolve, _reject) => {
+                                mdText.push(`# DataTable: ${param.name}`);
+                                if(!utils.isEmpty(param.comment)) {
+                                    mdText.push(param.comment);
+                                }
+                                this.query("selectTableStructure", { name: param.name })
+                                    .then((optData) => {
+                                        mdText.push(`|字段名|类型|注释|`);
+                                        mdText.push("|:-----|:-----|:-----|")
+                                        for(const row of optData) {
+                                            mdText.push(`|${row['COLUMN_NAME']}|${row['COLUMN_TYPE']}|${row['COLUMN_COMMENT']}|`);
+                                        }
+                                        mdText.push("--- ");
+                                        _resolve([]);
+                                    }).catch(_reject);
+                            });
+                        });
+                    }
+                }
+            ], undefined, {
+                throwException: true
+            }).then((resp) => {
+                fs.writeFileSync(fileName, mdText.join("\n"), { "encoding": "utf-8" });
+                resolve(resp.columns);
+            }).catch(reject);
+             
+        });
     }
     getNews() {
         
@@ -15,8 +91,6 @@ class LoginModel extends DataModel {
 @Controller("login")
 export class Login {
 
-    @Autowired()
-    loginModel: LoginModel;
     @Autowired()
     files: StaticFiles;
 
@@ -39,5 +113,9 @@ export class Login {
             maxAge: 10000
         });
         return this.files.readJson("./test.json");
+    }
+    @RequestMapping("/md","GET")
+    dbMarkdown(@GetDataModel(LoginModel) login: LoginModel):Promise<any> {
+        return login.dataSource();
     }
 }
